c81a0bf7bc0bb3b071910de2096c8c4877de03da
Charles Costa <chlima88@gmail.com>
2022-12-12 17:29:42 -0300

    Ajustes na classe EletricaTest

--- src/main/java/br/edu/infnet/atapp/model/tests/EletricaTest.java
+++ src/main/java/br/edu/infnet/atapp/model/tests/EletricaTest.java
@@ -14,33 +14,40 @@ public class EletricaTest {
 		// 3 - Circuito invalido -> Exibir erro
 		
 		try {
-			Eletrica e1 = new Eletrica("Revisão Eletrica", "L01", 100, false);
-			System.out.println("E1: " + e1);
+			System.out.println("\r\nTest case 1: Instanciar Eletrica com sucesso\r\n"
+					+ "Saida Esperada: classe instanciada\r\n");
+			Eletrica servico = new Eletrica("Revisão Eletrica", "L01", 100, false);
+			System.out.println(servico);
 		} catch (PrecoBaseInvalidoException error) {
-			System.out.println("E1: " + error.getMessage());
+			System.out.println("Servico: " + error.getMessage());
 		}
 		
 		
 		try {
-			Eletrica e2 = new Eletrica("Reparo circuito luzes", "L02", 350, true);
-			e2.setIncendio(false);
-			e2.setCircuitoDanificado("luzes");		
-			System.out.println("E2: " + e2);		
-			System.out.println("E2 Mao de obra: " + e2.obterPrecoMaoDeObra());
-			System.out.println("E2 Preco final: " + e2.obterPrecoFinal());
+			System.out.println("\r\nTest case 2: Validar o ajuste do preco devido a taxa de incendio\r\n"
+					+ "Saida Esperada: Maior preco final quando incendio é igual a true\r\n");
+			Eletrica servico = new Eletrica("Reparo circuito luzes", "L02", 350, true);
+			servico.setIncendio(false);
+			servico.setCircuitoDanificado("luzes");		
+			System.out.println("Servico (sem taxa): " + servico);		
+			System.out.println("Servico Mao de obra: " + servico.obterPrecoMaoDeObra());
+			System.out.println("Servico Preco final: " + servico.obterPrecoFinal());
+			servico.setIncendio(true);
+			System.out.println("Servico (com taxa):" + servico);		
+			System.out.println("Servico Mao de obra: " + servico.obterPrecoMaoDeObra());
+			System.out.println("Servico Preco final: " + servico.obterPrecoFinal());
 		} catch (PrecoBaseInvalidoException | CircuitoInvalidoException error) {
-			System.out.println("E2: " + error.getMessage());
+			System.out.println("Servico: " + error.getMessage());
 		}
 		
 		try {
-			Eletrica e3 = new Eletrica("Revisão circuito ignicao","L03",500, false);
-			e3.setIncendio(true);
-			e3.setCircuitoDanificado("ignicao");
-			System.out.println("E3: " + e3);
-			System.out.println("E3 Mao de obra: " + e3.obterPrecoMaoDeObra());
-			System.out.println("E3 Preco final: " + e3.obterPrecoFinal());
+			System.out.println("\r\nTest case 3: Exibir mensagem de erro quando informar um circuito invalido\r\n"
+					+ "Saida Esperada: mensagem de erro\r\n");
+			Eletrica servico = new Eletrica("Revisão circuito ignicao","L03",500, false);
+			servico.setIncendio(true);
+			servico.setCircuitoDanificado("abcd");
 		} catch (PrecoBaseInvalidoException | CircuitoInvalidoException error) {
-			System.out.println("E3: " + error.getMessage());
+			System.out.println("Servico: " + error.getMessage());
 		}
 		
 	}
