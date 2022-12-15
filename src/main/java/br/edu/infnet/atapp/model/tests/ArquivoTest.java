4f6d3bbe2f68b75eaae76db5909c2c71354b9c86
Charles Costa <chlima88@gmail.com>
2022-12-15 13:14:52 -0300

    CriAjustes na clÃasse ArquivoTest, versão original renomeada da classe ArquivoTest renomeada para ArquivoTestOld

--- src/main/java/br/edu/infnet/atapp/model/tests/ArquivoTest.java
+++ src/main/java/br/edu/infnet/atapp/model/tests/ArquivoTest.java
@@ -42,95 +42,100 @@ public class ArquivoTest {
 				String[] dados = null; 
 				
 				Agendamento agendamento = null;
-				List<Servico> servicos = new ArrayList<Servico>();
+				List<Servico> servicos = null;
 						
 				while (linha != null) {
 					
 					dados = linha.split(";");
 					
-					switch (dados[0].toUpperCase()) {
-					
-						case "A": 
-							try {
-								Cliente cliente = new Cliente(dados[3],dados[4],dados[5]);
-								agendamento = new Agendamento(cliente, servicos);
-								agendamento.setConfirmado("sim".equalsIgnoreCase(dados[2]) ? true : false );
-								agendamento.setDuracaoEmMinutos(Integer.valueOf(dados[1]));
-								
-							} catch (DuracaoAtendimentoException 
-									| ClienteIndefinidoException 
-									| ServicoIndefinidoException error) {
-								System.out.println("Agendamento: " + error.getMessage());
-							} catch (ClienteInvalidoException error) {
-								System.out.println("Cliente: " + error.getMessage());
-							} 						
-							break;
-							
-						case "L": 
-							try {
-								Lanternagem servico = new Lanternagem(
-										dados[1], 
-										dados[2], 
-										Integer.valueOf(dados[3]), 
-										Boolean.valueOf(dados[4])
-										);
-								servico.setLocalDanificado(dados[5]);
-								servico.setTamanhoAvaria(dados[6]);
-								
-								servicos.add(servico);		
-								
-							}catch (PrecoBaseInvalidoException 
-									| TamanhoInvalidoException error) {
-								System.out.println("Servico: " + error.getMessage());
-							} 
-							break;
-							
-						case "E": 
-							try {
-								Eletrica servico = new Eletrica(
-										dados[1], 
-										dados[2], 
-										Integer.valueOf(dados[3]), 
-										Boolean.valueOf(dados[4])
-										);
-								servico.setCircuitoDanificado((dados[5]));
-								servico.setIncendio(Boolean.valueOf(dados[6]));
-								
-								servicos.add(servico);
-
-							} catch (PrecoBaseInvalidoException  
-									| CircuitoInvalidoException error) {
-								System.out.println("Servico: " + error.getMessage());
-							} 
-							break;
-							
-						case "M": 
-							try {
-								Mecanica servico = new Mecanica(
-										dados[1], 
-										dados[2], 
-										Integer.valueOf(dados[3]), 
-										Boolean.valueOf(dados[4])
-										);
-								servico.setCategoriaServico(dados[5]);
-								servico.setProblemaMotor(Boolean.valueOf(dados[6]));
-								
-								servicos.add(servico);		
-								
-							}catch (PrecoBaseInvalidoException 
-									| CategoriaInvalidaException error) {
-								System.out.println("Servico: " + error.getMessage());
-							} 
-							break;
-							
-						default:
-							System.out.println("Registro invalido");
-							break;
+					try {
+						Cliente cliente = new Cliente(dados[3],dados[4],dados[5]);
+						servicos = new ArrayList<Servico>();
+						agendamento = new Agendamento(cliente, servicos);
+						agendamento.setConfirmado("sim".equalsIgnoreCase(dados[2]) ? true : false );
+						agendamento.setDuracaoEmMinutos(Integer.valueOf(dados[1]));
 						
+					} catch (DuracaoAtendimentoException 
+							| ClienteIndefinidoException 
+							| ServicoIndefinidoException error) {
+						System.out.println("Agendamento: " + error.getMessage());
+					} catch (ClienteInvalidoException error) {
+						System.out.println("Cliente: " + error.getMessage());
 					}
+					
 					linha = file.readLine();
+					if (linha != null) dados = linha.split(";");
+					
+					while(!dados[0].equalsIgnoreCase("A") && linha != null) {
+						
+						switch (dados[0].toUpperCase()) {
+							case "L": 
+								try {
+									Lanternagem servico = new Lanternagem(
+											dados[1], 
+											dados[2], 
+											Integer.valueOf(dados[3]), 
+											Boolean.valueOf(dados[4])
+											);
+									servico.setLocalDanificado(dados[5]);
+									servico.setTamanhoAvaria(dados[6]);
+									servicos.add(servico);		
+									
+								}catch (PrecoBaseInvalidoException 
+										| TamanhoInvalidoException error) {
+									System.out.println("Servico: " + error.getMessage());
+								} 
+								break;
+								
+							case "E": 
+								try {
+									Eletrica servico = new Eletrica(
+											dados[1], 
+											dados[2], 
+											Integer.valueOf(dados[3]), 
+											Boolean.valueOf(dados[4])
+											);
+									servico.setCircuitoDanificado((dados[5]));
+									servico.setIncendio(Boolean.valueOf(dados[6]));
+									servicos.add(servico);
+
+								} catch (PrecoBaseInvalidoException  
+										| CircuitoInvalidoException error) {
+									System.out.println("Servico: " + error.getMessage());
+								} 
+								break;
+								
+							case "M": 
+								try {
+									Mecanica servico = new Mecanica(
+											dados[1], 
+											dados[2], 
+											Integer.valueOf(dados[3]), 
+											Boolean.valueOf(dados[4])
+											);
+									servico.setCategoriaServico(dados[5]);
+									servico.setProblemaMotor(Boolean.valueOf(dados[6]));
+									servicos.add(servico);		
+									
+								}catch (PrecoBaseInvalidoException 
+										| CategoriaInvalidaException error) {
+									System.out.println("Servico: " + error.getMessage());
+								} 
+								break;
+								
+							default:
+								System.out.println("Registro invalido");
+								break;
+						}
+						
+						linha = file.readLine();
+						if (linha != null) dados = linha.split(";"); 
+					}
+					
+					System.out.println(agendamento.obterPedido());
+					fileW.write(agendamento.obterPedido()); 
+					
 				}
-				fileW.write(agendamento.obterPedido());
 				
 				fileW.close();
 				file.close();
