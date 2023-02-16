cc81a44a5426133b0c6375f9d92297e82a5d72c4
Charles Costa <chlima88@gmail.com>
2023-02-16 08:48:53 -0300

    Atualização do armazenamento de list para map,  e ajuste nos metodos psuporte, criação do excluir

--- src/main/java/br/edu/infnet/atapp/model/repository/UsuarioRepository.java
+++ src/main/java/br/edu/infnet/atapp/model/repository/UsuarioRepository.java
@@ -2,32 +2,54 @@ package br.edu.infnet.atapp.model.repository;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.edu.infnet.atapp.model.domain.Usuario;
 
 public class UsuarioRepository {
 	
-	private static List<Usuario> usuarios = new ArrayList<Usuario>();
+	//private static List<Usuario> usuarios = new ArrayList<Usuario>();
+	private static Map<Integer, Usuario> usuarios = new HashMap<Integer, Usuario>();
+	private static Integer id = 0;
 	
 	private static Usuario usuario = new Usuario("Usuario", "1@1.com", "123", null, "P", "1");
 	
 	static {
-		save(usuario);
+		try {
+			save(usuario);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 		
-	public static boolean save(Usuario usuario) {
+	public static void save(Usuario usuario) throws Exception {
 		
-		
-		usuarios.add(usuario);
-		return false;
+		try {
+			findByEmail(usuario.getEmail());			
+		} catch (Exception e) {
+			usuario.setId(id);
+			usuarios.put(id++, usuario);
+			return;
+		} 
+		throw new Exception("E-mail ja cadastrado!");
+
 	};
+	
+	public static Usuario delete(Integer key) throws Exception {
+		
+		Usuario usuario = usuarios.remove(key);
+		if (usuario == null) throw new Exception("O id informado é inválido ou não existe!");
+		return usuario;
+	}
 	
 	public static List<Usuario> findByName(String nome) {
 		
 		List<Usuario> usuariosEncontrados = new ArrayList<Usuario>();
 	
-		for (Usuario usuario: usuarios) {
+		for (Usuario usuario: usuarios.values()) {
 			if (usuario.getNome().toLowerCase().contains(nome.toLowerCase()) ) {
 				usuariosEncontrados.add(usuario);				
 			}
@@ -41,7 +63,7 @@ public class UsuarioRepository {
 		
 		Usuario usuarioEncontrado = null;
 	
-		for (Usuario usuario: usuarios) {
+		for (Usuario usuario: usuarios.values()) {
 			if (usuario.getEmail().equalsIgnoreCase(email.toLowerCase()) ) {
 				usuarioEncontrado = usuario;				
 			}
@@ -53,17 +75,25 @@ public class UsuarioRepository {
 		
 	};
 	
-	public static List<Usuario> findAll() {
-		return usuarios;
+	public static Collection<Usuario> findAll() {
+		return usuarios.values();
 	};
 	
-	public static void update(String email, Usuario usuario) throws Exception {
-		
-		Usuario usuarioEncontrado = findByEmail(email);
-		int usuarioIndex = usuarios.indexOf(usuarioEncontrado);
-		usuarios.set(usuarioIndex, usuario);	
-		
-	};
+//	public static void update(String email, Usuario usuario) throws Exception {
+//		
+//		Usuario usuarioEncontrado = findByEmail(email);
+//		
+//		try {
+//			if (email != usuario.getEmail()) findByEmail(usuario.getEmail());
+//		} catch(Exception error) {
+//			int usuarioIndex = usuarios.indexOf(usuarioEncontrado);
+//			usuarios.set(usuarioIndex, usuario);		
+//			return;
+//		}
+//		throw new Exception("O e-mail informado já está em uso!");
+//		
+//		
+//	};
 	
 	public static int count() {
 		return usuarios.size();
